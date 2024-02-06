815b6d1522fa8a5907046ee4206acb3bed058926
aakashbishnoi1 <8xaakash@gmail.com>
2024-02-06 17:17:11 +0530

    Add animation to cube

--- fundamentals/main.js
+++ fundamentals/main.js
@@ -6,7 +6,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({canvas: document.getElementById("bg")});
 
-camera.position.set(0,0,20);
+camera.position.set(0,0,40);
 renderer.pixelRatio = window.devicePixelRatio;
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -15,6 +15,17 @@ renderer.render(scene, camera);
 
 // Add a cube
 const cubeGeometry = new THREE.BoxGeometry(10,10,10);
-const cubeMaterial = new THREE.MeshStandardMaterial({color: 0x330587});
+const cubeMaterial = new THREE.MeshBasicMaterial({color: 0x330587});
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cube.position.set(-5,0);
 scene.add(cube);
+
+// animate
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+    renderer.render(scene, camera);
+}
+animate()
