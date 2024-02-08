dcc12efeeeb80dcf9693f0889ddb8f2fbee4c8ae
aakashbishnoi1 <8xaakash@gmail.com>
2024-02-08 13:15:15 +0530

    Resize renderer according to the screen

--- primitives/main.js
+++ primitives/main.js
@@ -9,3 +9,15 @@ const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({canvas: document.getElementById("bg")});
 
 renderer.render(scene, camera);
+// renderer resize according to screen  
+function resizeRendererToScreenSize(renderer) {
+    const canvas = renderer.domElement;
+    pixelRatio = window.devicePixelRatio;
+    const width = Math.floor(canvas.clientWidth * pixelRatio);
+    const height = Math.floor(canvas.clientHeight * pixelRatio);
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width,height,false);
+}
+    return needResize;
+}
