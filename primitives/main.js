08b2c394a206a17357b850513ee10ecbc96fd94b
aakashbishnoi1 <8xaakash@gmail.com>
2024-02-08 13:29:47 +0530

    Add a bed
    using boxGeometry

--- primitives/main.js
+++ primitives/main.js
@@ -7,6 +7,7 @@ import * as THREE from "three";
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({canvas: document.getElementById("bg")});
+camera.position.set(0,0,30);
 
 // renderer resize according to screen  
 function resizeRendererToScreenSize(renderer) {
@@ -20,6 +21,14 @@ function resizeRendererToScreenSize(renderer) {
 }
     return needResize;
 }
+
+// Add a bed using a box
+const bedGeometry = new THREE.BoxGeometry(10,2,4);
+const bedMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const bed = new THREE.Mesh(bedGeometry, bedMaterial);
+scene.add(bed);
+bed.position.set(1,3,0);
+
 
 function animate() {
     requestAnimationFrame(animate);
