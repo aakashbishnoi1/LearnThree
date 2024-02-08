188f64b974e90e1bf46cb8c4b4ba580764a67b97
aakashbishnoi1 <8xaakash@gmail.com>
2024-02-08 13:09:45 +0530

    set scene camera and renderer

--- primitives/main.js
+++ primitives/main.js
@@ -1,3 +1,11 @@
 import './style.css'
 
 import * as THREE from "three";
+
+// set up the camera, scene and renderer
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({canvas: document.getElementById("bg")});
+
+renderer.render(scene, camera);
